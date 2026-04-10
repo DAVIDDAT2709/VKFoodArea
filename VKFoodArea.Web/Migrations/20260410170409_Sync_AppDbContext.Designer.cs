ef0254da31a7c583002c0e1e1d251e90ab4c3c8d
NguyenManhHung-cntt <hung09052k5@gmail.com>
2026-04-11 00:08:28 +0700

    fix run Web

--- VKFoodArea.Web/Migrations/20260410170409_Sync_AppDbContext.Designer.cs
+++ VKFoodArea.Web/Migrations/20260410170409_Sync_AppDbContext.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VKFoodArea.Web.Data;
 
@@ -10,9 +11,11 @@ using VKFoodArea.Web.Data;
 namespace VKFoodArea.Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260410170409_Sync_AppDbContext")]
+    partial class Sync_AppDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
---
+++ VKFoodArea.Web/Migrations/20260410170409_Sync_AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VKFoodArea.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sync_AppDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserKey",
+                table: "NarrationHistories",
+                type: "TEXT",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserKey",
+                table: "NarrationHistories");
+        }
+    }
+}
